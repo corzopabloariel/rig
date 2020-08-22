1f02c856629f28b851eddf8f512e27b2c0213dd8
Pablo Corzo <corzo.pabloariel@gmail.com>
2020-08-22 14:41:14 -0300

    Primer commit

---
+++ database/migrations/2020_08_22_162721_create_texts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTextsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('texts', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 10)->unique();//Genera automática
+            $table->text('data')->nullable()->default(NULL);
+            $table->string('section', 100)->unique();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('texts');
+    }
+}
---
+++ database/migrations/2020_08_22_162739_create_statements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statements', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('operation_id');
+            $table->unsignedBigInteger('text_id');
+
+            $table->foreign('operation_id')->references('id')->on('operations')->onDelete('cascade');
+            $table->foreign('text_id')->references('id')->on('texts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statements');
+    }
+}
